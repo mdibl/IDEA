e9cc34b5dc4d1281dc7ede50acec7b204dd48007
Nathaniel <njmaki@mtu.edu>
2019-07-17 14:50:35 -0400

    add IDEA_mk1 python code

---
+++ IDEA_mk1.py
@@ -0,0 +1,6 @@
+#! /usr/bin/Rscript --vanilla --default-packages=utils
+import json
+import subprocess
+
+# subprocess to call the deseq2_exec.R script
+subprocess.call ("/Users/nmaki/Documents/GitHub/IDEA/deseq2_exec.R")
