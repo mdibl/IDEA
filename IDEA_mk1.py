e9771c49e2015557c1db1bb0402f2800026308af
Nathaniel <njmaki@mtu.edu>
2019-07-31 13:02:19 -0400

    update IDEA

--- IDEA_mk1.py
+++ IDEA_mk1.py
@@ -3,6 +3,7 @@ import csv
 import json
 import sys
 import urllib.request
+from time import sleep
 
 # read in csv file
 df = pd.read_csv(
@@ -15,37 +16,27 @@ log2FoldChange_sort.to_csv(
     index=False)
 
 string_api_url = "https://string-db.org/api"
-output_format = "tsv-no-header"
+output_format = "image"
 method = "network"
 
-my_genes = ["DPP", "W", "UBX", "N", "BRCA2"]
+my_genes = [["ENSDARG00000000002", "ENSDARG00000000018"],
+            ["ENSDARG00000000472", "ENSDARG00000000690"],
+            ["ENSDARG00000000853", "ENSDARG00000001057"]]
 
-species = "7227"
-my_app = "awesome_app.org"
+# use taxonomy ID for species
+species = "7955"
+my_app = "dev.azure.com/MDIBL"
 
-# construct request
-
+# create the request
 request_url = string_api_url + "/" + output_format + "/" + method + "?"
-request_url += "identifiers=%s" % "%0d".join(my_genes)
+request_url += "identifiers=%s"
 request_url += "&" + "species=" + species
+request_url += "&" + "add_white_nodes=30"
 request_url += "&" + "caller_identity=" + my_app
 
-try:
-    response = urllib.request.urlopen(request_url)
-except urllib.request.HTTPError as err:
-    error_message = err.read()
-    print (error_message)
-    sys.exit()
+# for each gene, call STRING
 
-# read and parse results
-
-line = response.readline()
-
-while line:
-    l = line.strip().split("\t")
-    p1, p2 = l[2], l[3]
-    experimental_score = float(l[10])
-    if experimental_score != 0:
-        print ("\t".join([p1,p2, "experimentally confirmed (prob. %.3f)" % experimental_score]))
-    
-    line = response.readline()
+for gene_pair in my_genes:
+    gene1, gene2 = gene_pair
+    urllib.request.urlretrieve(request_url % "%0d".join(gene_pair), "%s.png" % "_".join(gene_pair))
+    sleep(1)
