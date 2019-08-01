06762053dd8186e52f6bf894892db3f195a717cc
Nathaniel <njmaki@mtu.edu>
2019-08-01 10:42:23 -0400

    update and modularize IDEA and create thresholds json file

--- IDEA_mk1.py
+++ IDEA_mk1.py
@@ -4,6 +4,7 @@ import json
 import sys
 import urllib.request
 from time import sleep
+import string_call
 
 # read in csv file
 df = pd.read_csv(
@@ -15,28 +16,4 @@ log2FoldChange_sort.to_csv(
     "/Users/nmaki/Documents/GitHub/IDEA/DESeq2_genes_wPC_DESeq2out_edited.csv",
     index=False)
 
-string_api_url = "https://string-db.org/api"
-output_format = "image"
-method = "network"
-
-my_genes = [["ENSDARG00000000002", "ENSDARG00000000018"],
-            ["ENSDARG00000000472", "ENSDARG00000000690"],
-            ["ENSDARG00000000853", "ENSDARG00000001057"]]
-
-# use taxonomy ID for species
-species = "7955"
-my_app = "dev.azure.com/MDIBL"
-
-# create the request
-request_url = string_api_url + "/" + output_format + "/" + method + "?"
-request_url += "identifiers=%s"
-request_url += "&" + "species=" + species
-request_url += "&" + "add_white_nodes=30"
-request_url += "&" + "caller_identity=" + my_app
-
-# for each gene, call STRING
-
-for gene_pair in my_genes:
-    gene1, gene2 = gene_pair
-    urllib.request.urlretrieve(request_url % "%0d".join(gene_pair), "%s.png" % "_".join(gene_pair))
-    sleep(1)
+string_call.request()
---
+++ string_call.py
@@ -0,0 +1,28 @@
+import urllib.request
+from time import sleep
+
+def request(): 
+    string_api_url = "https://string-db.org/api"
+    output_format = "image"
+    method = "network"
+
+    my_genes = [["ENSDARG00000003495", "ENSDARG00000007245"],
+                ["ENSDARG00000000779", "ENSDARG00000000690"],
+                ["ENSDARG00000002909", "ENSDARG00000002790"]]
+
+    # use taxonomy ID for zebrafish
+    species = "7955"
+    my_app = "dev.azure.com/MDIBL"
+
+    # create the request
+    request_url = string_api_url + "/" + output_format + "/" + method + "?"
+    request_url += "identifiers=%s"
+    request_url += "&" + "species=" + species
+    request_url += "&" + "add_white_nodes=30"
+    request_url += "&" + "caller_identity=" + my_app
+
+    # for each gene, call STRING
+    for gene_pair in my_genes:
+        gene1, gene2 = gene_pair
+        urllib.request.urlretrieve(request_url % "%0d".join(gene_pair), "%s.png" % "_".join(gene_pair))
+        sleep(1)
