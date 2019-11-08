ef7159f5a2d0b83326225379e851581afa61a3c7
nathanielmki <njmaki@mtu.edu>
2019-11-08 15:24:35 -0500

    update core IDEA to pass argparse contents into df

--- IDEA.py
+++ IDEA.py
@@ -10,7 +10,10 @@ parser.add_argument('filename')
 args = parser.parse_args()
 with open(args.filename) as file:
     try:
-        print(yaml.safe_load(file))
+        #print(yaml.safe_load(file))
         # load in DESeq2 file from yaml
+        df = pd.io.json.json_normalize(yaml.safe_load(file))
+        print(df.head())
+        df = pd.read_csv('/Users/nmaki/Documents/GitHub/IDEA/tests/eDESeq2.csv')
     except yaml.YAMLError as exc:
         print(exc)
