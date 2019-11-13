7fc8cab34d59d01affb4c8e837ec824023616259
nathanielmki <njmaki@mtu.edu>
2019-11-13 16:01:55 -0500

    update IDEA with proper yaml loading component

--- IDEA.py
+++ IDEA.py
@@ -12,8 +12,12 @@ with open(args.filename) as file:
     try:
         #print(yaml.safe_load(file))
         # load in DESeq2 file from yaml
-        df = pd.io.json.json_normalize(yaml.safe_load(file))
-        print(df.head())
-        df = pd.read_csv('/Users/nmaki/Documents/GitHub/IDEA/tests/eDESeq2.csv')
+        # TODO: load yaml, create df from csv contents
+        # must work with argparse
+        #df = pd.io.json.json_normalize(yaml.safe_load(file))
+        #print(df.head())
+        config = yaml.full_load(file)
+        for item, doc in config.items():
+            print (item, ":", doc)
     except yaml.YAMLError as exc:
         print(exc)
