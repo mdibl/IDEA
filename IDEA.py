c06c5aa93e4e9198cdab43c0a073a7818709a1f3
nathanielmki <njmaki@mtu.edu>
2019-11-18 17:06:45 -0500

    update IDEA

--- IDEA.py
+++ IDEA.py
@@ -1,10 +1,9 @@
-#TODO: check for open success, if fail die
+# TODO: check for open success, if fail die
 import yaml
-import logging
 import argparse
 import pandas as pd
 
-# implement max_query_size(500) for now
+# TODO: implement max_query_size(500) for now
 # avoid overloading string-db
 
 # argparser for input file
@@ -13,32 +12,35 @@ parser.add_argument('filename')
 args = parser.parse_args()
 with open(args.filename) as file:
     try:
-        #print(yaml.safe_load(file))
+        # print(yaml.safe_load(file))
         # load in DESeq2 file from yaml
         # TODO: load yaml, create df from csv contents
         # must work with argparse
-        #df = pd.io.json.json_normalize(yaml.safe_load(file))
+        # df = pd.io.json.json_normalize(yaml.safe_load(file))
         config = yaml.full_load(file)
-        #for item, doc in config.items():
-            #print (item, ":", doc)
+        # for item, doc in config.items():
+        # print (item, ":", doc)
         input_path = config['DESeq_input']['path']
-        #print(input_path)
+        # print(input_path)
         df = pd.read_csv(input_path)
         baseMean = config['baseMean']
         log2FoldChange = config['log2FoldChange']
         lfcSE = config['lfcSE']
         pvalue = config['pvalue']
         padj = config['padj']
+        # print if 0 < than padj for test
+        # convert to #, most likely being read as string
         # now use threshold value to cut down CSV
-        thresh_df = df[['genes','baseMean','log2FoldChange','lfcSE','pvalue','padj']]
-        thresh_df.set_index('genes')
-        print(thresh_df)
-        thresh_val = thresh_df.loc[(thresh_df['baseMean'] < baseMean) 
-                                    & (thresh_df['log2FoldChange'] < log2FoldChange)
-                                    & (thresh_df['lfcSE'] < lfcSE) 
-                                    & (thresh_df['pvalue'] < pvalue) 
-                                    & (thresh_df['padj'] < padj)]
-        print(thresh_val)
+        # only columns defined in config.yaml file
+        df_threshold = df[['genes', 'baseMean', 'log2FoldChange', 'lfcSE', 'pvalue', 'padj']]
+        # print(df_threshold)
+        val_threshold = df_threshold.loc[(df_threshold['baseMean'] < baseMean)
+                                         & (df_threshold['log2FoldChange'] < log2FoldChange)
+                                         & (df_threshold['lfcSE'] < lfcSE)
+                                         & (df_threshold['pvalue'] < pvalue)
+                                         & (df_threshold['padj'] < padj)]
+        df_index = val_threshold.sort_index()
+        print(val_threshold)
     except yaml.YAMLError as exc:
         print(exc)
 
@@ -46,4 +48,3 @@ with open(args.filename) as file:
 # slice based upon names that pass primary threshhold
 # split passing and failing into separate tables
 # remove duplicates?
-
