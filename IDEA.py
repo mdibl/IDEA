9c5eb139a73e2c8f5888d9c31063ae1eb25a9dd4
nathanielmki <njmaki@mtu.edu>
2019-11-20 15:12:43 -0500

    add string-db fetcher code, add solitary python3 string-db test program

--- IDEA.py
+++ IDEA.py
@@ -2,8 +2,8 @@
 import yaml
 import argparse
 import pandas as pd
-import urllib.request
 import sys
+import urllib.request, urllib.error, urllib.parse
 from interaction_evidence import request
 
 # TODO: implement max_query_size(500) for now
@@ -15,6 +15,7 @@ parser.add_argument('filename')
 args = parser.parse_args()
 with open(args.filename) as file:
     try:
+        # bytes==str
         # print(yaml.safe_load(file))
         # load in DESeq2 file from yaml
         # TODO: load yaml, create df from csv contents
@@ -39,13 +40,48 @@ with open(args.filename) as file:
         # print(df_select)
         # print(df_select['genes'])
         df_threshold = df_select.loc[#(df_select['baseMean'] < baseMean)
-                                           (df_select['log2FoldChange'] < log2FoldChange)
-                                         #& (df_select['lfcSE'] < lfcSE)
+                                          (df_select['log2FoldChange'] < log2FoldChange)
+                                         & (df_select['lfcSE'] < lfcSE)
                                          & (df_select['pvalue'] < pvalue)
                                          & (df_select['padj'] < padj)]
         # df.index = df_select['genes']
         # df.set_index('genes', inplace=True)
         print(df_threshold)
+        # print(df_threshold['genes'])
+        # string-api call
+        string_api_url = "https://string-db.org/api"
+        output_format = "tsv-no-header"
+        method = "network"
+
+        my_genes = df_threshold['genes']
+        print(my_genes)
+        species = "7955"
+        my_app = "www.awesome_app.org"
+        # build request
+        request_url = string_api_url + "/" + output_format + "/" + method + "?"
+        request_url += "identifiers=%s" % "%0d".join(my_genes)
+        request_url += "&" + "species=" + species
+        request_url += "&" + "caller_identity=" + my_app
+
+        try:
+            response = urllib.request.urlopen(request_url)
+        except urllib.error.HTTPError as err:
+            error_message = err.read()
+            print(error_message)
+            sys.exit()
+        
+        # read parse results
+
+        line = response.readline()
+
+        while line:
+            l = line.strip().split("\t")
+            p1, p2 = l[2], l[3]
+            experimental_score = float(l[10])
+            if experimental_score != 0:
+                print("\t".join([p1,p2, "experimentally confirmed (prob. %.3f)" % experimental_score]))
+            
+            line = response.readline()
     except yaml.YAMLError as exc:
         print(exc)
 
