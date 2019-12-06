bff84d1edc63a14913fe83794b58d53f7c02c9e8
nathanielmki <njmaki@mtu.edu>
2019-12-06 15:25:58 -0500

    add non-functional id match function

--- IDEA.py
+++ IDEA.py
@@ -54,6 +54,35 @@ with open(args.filename) as file:
         # print(df_threshold)
         # print(df_threshold['genes'])
         # for each protein in a given list, print scores of experimental significance
+
+
+        # fix TypeError: sequence item 0: expected str, series found
+
+        def fetch_id():
+            string_api_url = "https://string-db.org/api"
+            output_format = "tsv-no-header"
+            method = "get_string_ids"
+
+            # configure parameters
+            params = {
+
+                "identifiers" : "\r".join([my_genes]),
+                "species" : species_id,
+                "limit" : 1,
+                "echo_query" : 1,
+                "caller_identity" : "www.awesome_app.org"
+            }
+
+            request_url = "/".join([string_api_url, output_format, method])
+            
+            results = requests.post(request_url, data=params)
+
+            for line in results.text.strip().split("\n"):
+                l = line.split("\t")
+                input_identifier, string_identifier = l[0], l[2]
+                print("Input:", input_identifier, "STRING:", string_identifier, sep="\t")
+        fetch_id()
+
         def network():
             string_api_url = "https://string-db.org/api"
             output_format = "tsv-no-header"
